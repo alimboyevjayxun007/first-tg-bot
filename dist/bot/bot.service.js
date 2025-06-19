1092b487d37c6a6404e44ce7ed560f98390a114c
alimboyevjayxun007 <alimboyevjayxun007@gmail.com>
2025-06-19 13:19:29 +0500

    fast food bot

--- dist/bot/bot.service.js
+++ dist/bot/bot.service.js
@@ -11,7 +11,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BotService = void 0;
 const common_1 = require("@nestjs/common");
@@ -466,6 +465,6 @@ exports.BotService = BotService;
 exports.BotService = BotService = __decorate([
     (0, common_1.Injectable)(),
     __param(0, (0, mongoose_1.InjectModel)(bot_schema_1.User.name)),
-    __metadata("design:paramtypes", [typeof (_a = typeof mongoose_2.Model !== "undefined" && mongoose_2.Model) === "function" ? _a : Object])
+    __metadata("design:paramtypes", [mongoose_2.Model])
 ], BotService);
 //# sourceMappingURL=bot.service.js.map
--- dist/schema/bot.schema.d.ts
+++ dist/schema/bot.schema.d.ts
@@ -6,4 +6,12 @@ export declare class User {
     phoneNumber?: string;
     createdAt: Date;
 }
-export declare const UserSchema: any;
+export declare const UserSchema: import("mongoose").Schema<User, import("mongoose").Model<User, any, any, any, import("mongoose").Document<unknown, any, User, any> & User & {
+    _id: import("mongoose").Types.ObjectId;
+} & {
+    __v: number;
+}, any>, {}, {}, {}, {}, import("mongoose").DefaultSchemaOptions, User, import("mongoose").Document<unknown, {}, import("mongoose").FlatRecord<User>, {}> & import("mongoose").FlatRecord<User> & {
+    _id: import("mongoose").Types.ObjectId;
+} & {
+    __v: number;
+}>;
